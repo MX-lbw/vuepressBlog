7d453ea4beed9193491e37b4c48dd280cf89cc25
wuwei <963337934@qq.com>
2023-02-26 15:49:51 +0800

    第二次提交整体框架搭可以写文章了建

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,5 @@
 const nav = require("./config/nav");
+const htmlModules = require("./config/htmlModules.js");
 module.exports = {
   base: "/",
   title: "碌碌无为-知识整理",
@@ -22,14 +23,49 @@ module.exports = {
     nav,
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: "/img/leftLogo.ico", // 导航栏logo
-    repo: "xiaoxian521/vue-pure-admin", // 导航栏右侧生成Github链接
+    repo: "MX-lbw/vuepressBlog", // 导航栏右侧生成Github链接
+    searchMaxSuggestions: 10, // 搜索结果显示最大数
+    lastUpdated: "上次更新", // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
     searchPlaceholder: "快速搜索",
     sidebar: { mode: "structuring", collapsable: false }, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果您不设置为'structuring',将无法使用目录页
+    category: false,
+    tag: false,
+    markdown: {
+      lineNumbers: true, // 显示代码块的行号
+      extractHeaders: ["h2", "h3", "h4", "h5", "h6"], // 提取标题到侧边栏的级别，默认['h2', 'h3']
+    },
+    updateBar: {
+      // 最近更新栏
+      showToArticle: false, // 显示到文章页底部，默认true
+      // moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
+    },
+    author: {
+      // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, href: String}
+      name: "wuwei", // 必需
+      href: "https://github.com/MX-lbw", // 可选的
+    },
+    social: {
+      // 社交图标，显示于博主信息栏和页脚栏
+      // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
+      icons: [
+        {
+          iconClass: "icon-github",
+          title: "GitHub",
+          link: "https://github.com/MX-lbw",
+        },
+        {
+          iconClass: "icon-bilibili",
+          title: "bilibili",
+          link: "https://space.bilibili.com/301103230",
+        },
+      ],
+    },
     footer: {
       // 页脚信息
       createYear: 2023, // 博客创建年份
       copyrightInfo: "RealityBoy | MIT License", // 博客版权信息，支持a标签
     },
+    htmlModules,
   },
   plugins: [["@vuepress/nprogress"], ["reading-progress"]],
 };
--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -2,60 +2,53 @@ module.exports = [
   { text: "首页", link: "/" },
   {
     text: "前端",
-    link: "/pages/introduction/",
+    link: "/pages/html/",
     items: [
-      { text: "Html", link: "/pages/introduction/" },
-      { text: "Css", link: "/pages/start/" },
-      { text: "JavaScript", link: "/pages/directory/" },
-      { text: "Less", link: "/pages/vscode/" },
+      { text: "Html", link: "/pages/html/" },
+      { text: "Css", link: "/pages/css/" },
+      { text: "Less", link: "/pages/less/" },
       {
         text: "JavaScript",
-        items: [],
+        link: "/pages/javaScript/",
+        items: [{ text: "Es6", link: "/pages/es6/" }],
       },
       {
         text: "TypeScript",
-        items: [],
+        items: [{ text: "Es6", link: "/pages/es6/" }],
       },
       {
         text: "Vue2.x",
-        items: [],
+        items: [{ text: "Vue2.x", link: "/pages/vue2.x/" }],
       },
       {
         text: "Vue3.x",
-        items: [],
+        items: [{ text: "Vue3.x", link: "/pages/vue3.x/" }],
       },
       {
         text: "Pinia",
-        items: [],
+        items: [{ text: "Pinia", link: "/pages/pinia/" }],
       },
       {
         text: "Vite",
-        items: [],
+        items: [{ text: "Vite", link: "/pages/vite/" }],
       },
       {
         text: "小程序",
-        items: [],
-      },
-      {
-        text: "git",
-        items: [],
+        items: [{ text: "小程序", link: "/pages/xiaochengxu/" }],
       },
     ],
   },
   {
     text: "Node",
+    link: "/pages/koa/",
     items: [
-      {},
-      {
-        text: "Github源码",
-        items: [
-          {
-            text: "完整版",
-            link: "https://github.com/pure-admin/vue-pure-admin",
-          },
-        ],
-      },
+      { text: "Koa", link: "/pages/koa/" },
+      { text: "express", link: "/pages/express/" },
     ],
+  },
+  {
+    text: "Git",
+    link: "/pages/git/",
   },
   {
     text: "面试题",
